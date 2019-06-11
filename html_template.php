a57d80adce8f30d1473b28552960be34a8130c72
Dennis Muensterer <dennismuensterer@gmail.com>
2019-06-11 13:52:22 +0200

    new index and template

---
+++ html_template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+	<meta charset="utf-8">
+	<title>Dies ist das Minimum HTML5 Template</title>
+	<meta name="description" content="Eine Beschreibung der Seite">
+	<meta name="author" content="Dennis Muensterer">
+	<link rel="stylesheet" href="/css/styles.css?v=1.0">
+	<script src="js/scripts.js"></script>
+</head>
+
+<body>
+	<h1>Hallo Welt!</h1>
+	<p>Lorem ipsum...</p>
+</body>
+
+</html>
---
+++ index.md
@@ -0,0 +1,4 @@
+# Dennis Muensterer
+
+# Lebenslauf
+As an assignment, I made an [HTML version of my CV](lebenslauf.html)
